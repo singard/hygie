bacf1b6512a8352707e4f1b3907604e42aaf648a
gonza <gonza@LAPTOP-Q6VLQI4T>
2023-05-30 19:02:14 +0200

    ram ok

--- hygie-tests/src/main/java/com/hygie/Main.java
+++ hygie-tests/src/main/java/com/hygie/Main.java
@@ -12,14 +12,22 @@ public class Main {
 
 	public static void main(String[] args) throws IllegalAccessException, InstantiationException {
 		
-		ExecuteTask executeTask = new ExecuteTask(TaskType.CHECK_FREE_SPACE_DISC,"free space up to 50 Go", new String[] {"50"} );
-		ResultTask resultTask = ResearchTest.executerClasse(executeTask);
-		log.info(resultTask.toString());
+//		ExecuteTask executeTask = new ExecuteTask(TaskType.CHECK_FREE_SPACE_DISC,"free space up to 50 Go", new String[] {"50"} );
+//		ResultTask resultTask = ResearchTest.executerClasse(executeTask);
+//		log.info(resultTask.toString());
+//		
+//		ExecuteTask executeTask500 = new ExecuteTask(TaskType.CHECK_FREE_SPACE_DISC,"free space up to 500 Go", new String[] {"500"} );
+//		ResultTask resultTask500 = ResearchTest.executerClasse(executeTask500);
+//		log.info(resultTask500.toString());
 		
-		ExecuteTask executeTask500 = new ExecuteTask(TaskType.CHECK_FREE_SPACE_DISC,"free space up to 500 Go", new String[] {"500"} );
-		ResultTask resultTask500 = ResearchTest.executerClasse(executeTask500);
-		log.info(resultTask500.toString());
-
+		ExecuteTask executeTaskRAM50 = new ExecuteTask(TaskType.CHECK_TOTAL_RAM,"RAM up to 50 Go", new String[] {"50"} );
+		ResultTask resultTaskRAM50 = ResearchTest.executerClasse(executeTaskRAM50);
+		log.info(resultTaskRAM50.toString());
+		
+		ExecuteTask executeTaskRAM5 = new ExecuteTask(TaskType.CHECK_TOTAL_RAM,"RAM up to 5 Go", new String[] {"5"} );
+		ResultTask resultTaskRAM5 = ResearchTest.executerClasse(executeTaskRAM5);
+		log.info(resultTaskRAM5.toString());
+		
 	}
 	
 	
