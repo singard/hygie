956363b7ea209e87d3d2345a6e475d9797bea7f3
Victor <victor.rolland.leonetti@gmail.com>
2023-06-13 23:20:59 +0200

    Ajout de l'execution des taches depuis le JSON

--- hygie-json/src/main/java/com/hygie/research/ReadFile.java
+++ hygie-json/src/main/java/com/hygie/research/ReadFile.java
@@ -2,12 +2,17 @@ package com.hygie.research;
 
 import java.io.File;
 import java.io.FileReader;
+
+import com.hygie.model.ExecuteTask;
+import com.hygie.model.ResultTask;
+import com.hygie.model.TaskType;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 public class ReadFile {
-
     public static void read(File file) {
         try {
             // Créer un parser JSON
@@ -23,17 +28,33 @@ public class ReadFile {
             for (Object o : jsonArray) {
                 JSONObject jsonObject = (JSONObject) o;
 
-                // Récupérer les valeurs de testType et description
+                // Récupérer les valeurs de testType, description et args
                 String testType = (String) jsonObject.get("testType");
                 String description = (String) jsonObject.get("description");
+                JSONArray argsArray = (JSONArray) jsonObject.get("args");
 
-                // Afficher les valeurs
-                System.out.println("Test Type: " + testType);
-                System.out.println("Description: " + description);
-                System.out.println();
+                // Convertir les arguments en tableau de chaînes
+                String[] args = new String[argsArray.size()];
+                for (int i = 0; i < argsArray.size(); i++) {
+                    args[i] = (String) argsArray.get(i);
+                }
+
+                // Vérifier le type de test et exécuter la tâche correspondante
+                if (testType.equals("CHECK_TOTAL_RAM")) {
+                    ExecuteTask executeTask = new ExecuteTask(TaskType.CHECK_TOTAL_RAM, description, args);
+                    ResultTask resultTask = ResearchTest.executerClasse(executeTask);
+                    log.info(resultTask.toString());
+                } else if (testType.equals("CHECK_FREE_SPACE_DISC")) {
+                    ExecuteTask executeTask = new ExecuteTask(TaskType.CHECK_FREE_SPACE_DISC, description, args);
+                    ResultTask resultTask = ResearchTest.executerClasse(executeTask);
+                    log.info(resultTask.toString());
+                } else {
+                    log.error("TestType non pris en charge : " + testType);
+                }
             }
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
+
 }
