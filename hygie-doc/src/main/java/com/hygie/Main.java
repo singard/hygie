b4cf0255a371a5d48db98aaaca7ec97ada1e88e7
Victor <victor.rolland.leonetti@gmail.com>
2023-06-15 11:50:48 +0200

    Ajout de la fonction PdfCreator qui créer un fichier pdf avec un titre

---
+++ hygie-doc/src/main/java/com/hygie/Main.java
@@ -0,0 +1,13 @@
+package com.hygie;
+
+import com.hygie.export.PdfCreator;
+
+public class Main {
+    public static void main(String[] args) {
+        String title = "My PDF Title";
+        String fileName = "filetest.pdf";
+
+        PdfCreator pdfCreator = new PdfCreator(title, fileName);
+        pdfCreator.createPdf();
+    }
+}
