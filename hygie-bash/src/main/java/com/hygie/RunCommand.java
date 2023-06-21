175f220b22c46e8a891b6942cd22b095f0317d02
gonza <gonza@LAPTOP-Q6VLQI4T>
2023-06-21 22:26:59 +0200

    correction lecture du json en beaucoup plus court

--- hygie-bash/src/main/java/com/hygie/RunCommand.java
+++ hygie-bash/src/main/java/com/hygie/RunCommand.java
@@ -3,9 +3,12 @@ package com.hygie;
 import java.io.File;
 import java.nio.file.Paths;
 
+import org.jline.utils.Log;
 import org.springframework.shell.standard.ShellComponent;
 import org.springframework.shell.standard.ShellMethod;
 import org.springframework.shell.standard.ShellOption;
+
+import com.hygie.research.ReadFile;
 
 @ShellComponent
 public class RunCommand {
@@ -14,9 +17,15 @@ public class RunCommand {
     public String run(@ShellOption(help = "File name") String fileName) {
     	String jarPath = getJarPath();
         String folderPath = Paths.get(jarPath).getParent().toString();
+        
         String filePath = Paths.get(folderPath, fileName).toString();
-
+        Log.info("Running file: " + filePath);
         // Implémentez ici la logique pour exécuter le fichier spécifié
+
+        ReadFile.read(new File("filePath"));
+        //lecture du fichier et transformation en ExecuteTask
+        
+        //génération du raport
         return "Running file: " + filePath;
     }
 
--- hygie-json/src/main/java/com/hygie/research/ReadFile.java
+++ hygie-json/src/main/java/com/hygie/research/ReadFile.java
@@ -1,5 +1,6 @@
 package com.hygie.research;
 
+import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
 
@@ -11,72 +12,37 @@ import java.util.List;
 import com.hygie.model.ExecuteTask;
 import com.hygie.model.ResultTask;
 import com.hygie.model.TaskType;
-import org.json.simple.JSONArray;
-import org.json.simple.JSONObject;
-import org.json.simple.parser.JSONParser;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 public class ReadFile {
-    public static void read(File file) {
-        try {
-            // Cr√©er un parser JSON
-            JSONParser parser = new JSONParser();
-
-            // Lire le fichier JSON
-            Object obj = parser.parse(new FileReader(file));
-
-            // Convertir en tableau JSON
-            JSONArray jsonArray = (JSONArray) obj;
-
-            // Parcourir les objets du tableau
-            for (Object o : jsonArray) {
-                JSONObject jsonObject = (JSONObject) o;
-
-                // R√©cup√©rer les valeurs de testType, description et args
-                String testType = (String) jsonObject.get("testType");
-                String description = (String) jsonObject.get("description");
-                JSONArray argsArray = (JSONArray) jsonObject.get("args");
-
-                // Convertir les arguments en tableau de cha√Ænes
-                String[] args = new String[argsArray.size()];
-                for (int i = 0; i < argsArray.size(); i++) {
-                    args[i] = (String) argsArray.get(i);
-                }
-
-                // V√©rifier le type de test et ex√©cuter la t√¢che correspondante
-                if (testType.equals("CHECK_TOTAL_RAM")) {
-                    ExecuteTask executeTask = new ExecuteTask(TaskType.CHECK_TOTAL_RAM, description, args);
-                    ResultTask resultTask = ResearchTest.executerClasse(executeTask);
-                    log.info(resultTask.toString());
-                } else if (testType.equals("CHECK_FREE_SPACE_DISC")) {
-                    ExecuteTask executeTask = new ExecuteTask(TaskType.CHECK_FREE_SPACE_DISC, description, args);
-                    ResultTask resultTask = ResearchTest.executerClasse(executeTask);
-                    log.info(resultTask.toString());
-                } else {
-                    log.error("TestType non pris en charge : " + testType);
-                }
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
+	 public static void read(File file) {
+	        ObjectMapper objectMapper = new ObjectMapper();
+	        try {
+	            List<ExecuteTask> executeTasks = objectMapper.readValue(file, new TypeReference<List<ExecuteTask>>() {});
+	            // Faites ce que vous voulez avec la liste d'objets ExecuteTask
+	            for (ExecuteTask task : executeTasks) {
+	                System.out.println(task);
+	            }
+	        } catch (IOException e) {
+	            e.printStackTrace();
+	        }
+	    }
 
     public static void createJsonFile(String fileName, List<ExecuteTask> taskList) {
         try {
-            // Cr√©er un objet ObjectMapper
+            // CrÈer un objet ObjectMapper
             ObjectMapper objectMapper = new ObjectMapper();
 
-            // Activer la mise en forme du JSON pour une meilleure lisibilit√©
+            // Activer la mise en forme du JSON pour une meilleure lisibilitÈ
             objectMapper.enable(SerializationFeature.INDENT_OUTPUT);
 
-            // Convertir la liste de t√¢ches en JSON et √©crire dans le fichier
+            // Convertir la liste de t‚ches en JSON et Ècrire dans le fichier
             objectMapper.writeValue(new File(fileName), taskList);
 
-            System.out.println("Le fichier JSON '" + fileName + "' a √©t√© cr√©√© avec succ√®s.");
+            System.out.println("Le fichier JSON '" + fileName + "' a ÈtÈ crÈÈ avec succËs.");
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
-
 }
