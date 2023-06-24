905cf9b7de16f138557d5505accbee2afd01544e
gonza <gonza@LAPTOP-Q6VLQI4T>
2023-06-24 11:06:42 +0200

    package windows sans java ok mais correction de hygie-bash pour le dossier data

--- hygie-installer/src/main/windows/ivtracer-install-checker/IVTracerInstallChecker.bat
+++
@@ -1,2 +0,0 @@
-${javaPathWindows} -jar -Xmx1024m ivtracer-install-checker-core-${project.version}.jar --logging.config=logback.xml --spring.config.name=application,mapping --spring.config.location=./
-pause
