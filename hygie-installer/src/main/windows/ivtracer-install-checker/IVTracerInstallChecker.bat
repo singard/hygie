bb4953190ea88d7423a6ac9e04b101273426872e
gonza <gonza@LAPTOP-Q6VLQI4T>
2023-06-24 10:37:02 +0200

    add file

---
+++ hygie-installer/src/main/windows/ivtracer-install-checker/IVTracerInstallChecker.bat
@@ -0,0 +1,2 @@
+${javaPathWindows} -jar -Xmx1024m ivtracer-install-checker-core-${project.version}.jar --logging.config=logback.xml --spring.config.name=application,mapping --spring.config.location=./
+pause
--- hygie-installer/target/hygie-installer-0.0.1-SNAPSHOT-windows/ivtracer-install-checker/IVTracerInstallChecker.bat
+++
@@ -1,2 +0,0 @@
-java\bin\java.exe -jar -Xmx1024m ivtracer-install-checker-core-0.0.1-SNAPSHOT.jar --logging.config=logback.xml --spring.config.name=application,mapping --spring.config.location=./
-pause
