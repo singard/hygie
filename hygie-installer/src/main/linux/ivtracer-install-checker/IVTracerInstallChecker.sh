44e998b13eeb4b54a9bef7f029ba831aebd5cddc
gonza <gonza@LAPTOP-Q6VLQI4T>
2023-06-26 22:32:02 +0200

    clean

--- hygie-installer/src/main/linux/ivtracer-install-checker/IVTracerInstallChecker.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/su root
-${javaPathLinux} -jar -Xmx1024m ivtracer-install-checker-core-${project.version}.jar --logging.config=logback.xml --spring.config.name=application,mapping --spring.config.location=./
