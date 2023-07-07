7186fde71873b07de19e616ba1ba967ff8ae630c
gonza <gonza@LAPTOP-Q6VLQI4T>
2023-07-07 19:11:47 +0200

    création du serveur https avec certificat autosigner jsk crée par les outils java + passer en java 16 pour fonctionner

--- hygie-task/src/main/java/com/hygie/hygietask/model/TaskType.java
+++ hygie-task/src/main/java/com/hygie/hygietask/model/TaskType.java
@@ -2,6 +2,7 @@ package com.hygie.hygietask.model;
 
 import com.hygie.hygietask.tasks.CheckFreeSpaceDisc;
 import com.hygie.hygietask.tasks.CheckTotalRam;
+import com.hygie.hygietask.tasks.CreateHTTPSServer;
 import com.hygie.hygietask.tasks.CreateHTTPServer;
 
 import lombok.AllArgsConstructor;
@@ -11,7 +12,8 @@ public enum TaskType {
 	
 	CHECK_FREE_SPACE_DISC(CheckFreeSpaceDisc.class),
 	CHECK_TOTAL_RAM(CheckTotalRam.class), 
-	CREATE_HTTP_SERVER(CreateHTTPServer.class);
+	CREATE_HTTP_SERVER(CreateHTTPServer.class),
+	CREATE_HTTPS_SERVER(CreateHTTPSServer.class);
 	
 	private final Class<? extends TaskClass> classe;
 
