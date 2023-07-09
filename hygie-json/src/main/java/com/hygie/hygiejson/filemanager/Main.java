7098bfe35ac267d24ba22c799b548513337c1656
Victor <victor.rolland.leonetti@gmail.com>
2023-07-09 23:15:51 +0200

    Correction du chemin d'accès de fiche01.json

--- hygie-json/src/main/java/com/hygie/hygiejson/filemanager/Main.java
+++ hygie-json/src/main/java/com/hygie/hygiejson/filemanager/Main.java
@@ -1,33 +1,32 @@
 package com.hygie.hygiejson.filemanager;
 
-import static com.hygie.hygiejson.filemanager.ReadFile.createJsonFile;
-import static com.hygie.hygiejson.filemanager.ReadFile.read;
-
-import java.io.File;
-import java.util.Arrays;
-import java.util.List;
-
 import com.hygie.hygietask.model.ExecuteTask;
 import com.hygie.hygietask.model.ResultTask;
 import com.hygie.hygietask.model.TaskType;
-import com.hygie.hygietask.research.ResearchTest;
+import java.io.File;
+import java.util.Arrays;
+import java.util.List;
+import static com.hygie.hygiejson.filemanager.ReadFile.createJsonFile;
+import static com.hygie.hygiejson.filemanager.ReadFile.read;
 
 public class Main {
     public static void main(String[] args) {
     	String DEFAULT_PLAYBOOK_PATH = System.getProperty("user.dir");
 
 		DEFAULT_PLAYBOOK_PATH = DEFAULT_PLAYBOOK_PATH.concat(File.separator)
-				.concat("data")
+				.concat("hygie-json")
 				.concat(File.separator)
+                .concat("data")
+                .concat(File.separator)
 				.concat("fiche01").concat(".json");
-		
+
         File file = new File(DEFAULT_PLAYBOOK_PATH);
-        
+
         List<ResultTask> resultTasks = read(file);
         for (ResultTask task : resultTasks) {
-            
+
             System.out.println(task);
-    		
+
         }
 
         // Créer une liste immuable de tâches
