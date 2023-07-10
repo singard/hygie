3dafec09bd4f32e44d46f0497c52756cee8ed4f0
Victor <victor.rolland.leonetti@gmail.com>
2023-07-10 13:36:07 +0200

    Ajout de la mise en page du fichier pdf

--- hygie-json/src/main/java/com/hygie/hygiejson/filemanager/Main.java
+++ hygie-json/src/main/java/com/hygie/hygiejson/filemanager/Main.java
@@ -4,11 +4,12 @@ import com.hygie.export.PdfCreator;
 import com.hygie.hygietask.model.ExecuteTask;
 import com.hygie.hygietask.model.ResultTask;
 import com.hygie.hygietask.model.TaskType;
+
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.Arrays;
 import java.util.List;
+
 import static com.hygie.hygiejson.filemanager.ReadFile.createJsonFile;
 import static com.hygie.hygiejson.filemanager.ReadFile.read;
 
@@ -31,7 +32,6 @@ public class Main {
             System.out.println(task);
 
         }
-
         String title = "Résultats des tests";
         String filePath = "result_tasks.pdf";
 
