3c8001facb6187bc2fcf1a495ec444886c1c1110
Victor <victor.rolland.leonetti@gmail.com>
2023-07-10 12:06:00 +0200

    ajout de l'affichage des resultats dans un fichier pdf

--- hygie-doc/src/main/java/com/hygie/export/PdfCreator.java
+++ hygie-doc/src/main/java/com/hygie/export/PdfCreator.java
@@ -1,23 +1,28 @@
 package com.hygie.export;
 
+import com.hygie.hygietask.model.ResultTask;
 import org.apache.pdfbox.pdmodel.PDDocument;
 import org.apache.pdfbox.pdmodel.PDPage;
 import org.apache.pdfbox.pdmodel.common.PDRectangle;
 import org.apache.pdfbox.pdmodel.PDPageContentStream;
-import org.apache.pdfbox.pdmodel.font.PDType1Font;
+import org.apache.pdfbox.pdmodel.font.PDType0Font;
 
-import java.io.IOException;
+import java.io.*;
+import java.util.List;
 
 public class PdfCreator {
-    private String title;
-    private String filePath;
+    private final String title;
+    private final String filePath;
+    String fontPath = "C:\\WINDOWS\\FONTS\\times.ttf";
+    File fontFile = new File(fontPath);
+    InputStream inputStream = new FileInputStream(fontFile);
 
-    public PdfCreator(String title, String filePath) {
+    public PdfCreator(String title, String filePath) throws FileNotFoundException {
         this.title = title;
         this.filePath = filePath;
     }
 
-    public void createPdf() {
+    public void createPdf(List<ResultTask> tasks) {
         try {
             PDDocument document = new PDDocument();
             PDPage page = new PDPage(PDRectangle.A4);
@@ -25,12 +30,25 @@ public class PdfCreator {
 
             PDPageContentStream contentStream = new PDPageContentStream(document, page);
 
-            contentStream.setFont(PDType1Font.HELVETICA_BOLD, 20);
+            PDType0Font font = PDType0Font.load(document, inputStream);
+
+            contentStream.setFont(font, 20);
             contentStream.beginText();
             contentStream.newLineAtOffset(50, 700);
             contentStream.showText(title);
             contentStream.endText();
 
+            contentStream.setFont(font, 12);
+            contentStream.beginText();
+            contentStream.newLineAtOffset(50, 650);
+
+            for (ResultTask task : tasks) {
+                String taskString = replaceUnsupportedCharacters(task.toString());
+                contentStream.showText(taskString);
+                contentStream.newLine();
+            }
+
+            contentStream.endText();
             contentStream.close();
 
             document.save(filePath);
@@ -42,5 +60,8 @@ public class PdfCreator {
         }
     }
 
-
+    private String replaceUnsupportedCharacters(String text) {
+        // Replace the unsupported character � with a suitable replacement
+        return text.replace("\uFFFD", "[Unsupported Character]");
+    }
 }
--- hygie-json/src/main/java/com/hygie/hygiejson/filemanager/Main.java
+++ hygie-json/src/main/java/com/hygie/hygiejson/filemanager/Main.java
@@ -1,16 +1,18 @@
 package com.hygie.hygiejson.filemanager;
 
+import com.hygie.export.PdfCreator;
 import com.hygie.hygietask.model.ExecuteTask;
 import com.hygie.hygietask.model.ResultTask;
 import com.hygie.hygietask.model.TaskType;
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.Arrays;
 import java.util.List;
 import static com.hygie.hygiejson.filemanager.ReadFile.createJsonFile;
 import static com.hygie.hygiejson.filemanager.ReadFile.read;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {
     	String DEFAULT_PLAYBOOK_PATH = System.getProperty("user.dir");
 
 		DEFAULT_PLAYBOOK_PATH = DEFAULT_PLAYBOOK_PATH.concat(File.separator)
@@ -29,6 +31,12 @@ public class Main {
 
         }
 
+        String title = "My PDF Title";
+        String filePath = "result_tasks.pdf";
+
+        PdfCreator pdfCreator = new PdfCreator(title, filePath);
+        pdfCreator.createPdf(resultTasks);
+
         // Créer une liste immuable de tâches
         List<ExecuteTask> taskList = Arrays.asList(
                 new ExecuteTask(TaskType.CHECK_FREE_SPACE_DISC, "Description 1", new String[]{"arg1", "arg2"}),
@@ -37,5 +45,7 @@ public class Main {
 
         // Appeler la fonction pour créer le fichier JSON
         createJsonFile("output.json", taskList);
+
+
     }
 }
